466f3a87d68079b52fcb4bef9b07ae434e707b69
Arey125 <andrej.kurenckoff@gmail.com>
2025-03-05 00:14:37 +0300

    feat: session storage

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,36 +11,52 @@ import (
 
 	database "lichess-arena/internal/db"
 	"lichess-arena/internal/users"
+
+	"github.com/gorilla/sessions"
 )
 
 func main() {
 	dsn := os.Getenv("DB")
-	db := database.Connect(dsn)
-	port, err := strconv.Atoi(os.Getenv("PORT"))
-	if err != nil {
-        panic(err);
+	if dsn == "" {
+		panic("DB environment variable is not set")
 	}
 
-    mux := http.NewServeMux()
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		panic(err)
+	}
+	if port == 0 {
+		panic("PORT environment variable is not set")
+	}
+
+    storageSecret := os.Getenv("STORAGE_SECRET")
+    if storageSecret == "" {
+        panic("STORAGE_SECRET environment variable is not set")
+    }
+
+	db := database.Connect(dsn)
+    sessionStore := sessions.NewCookieStore([]byte(storageSecret))
+
+	mux := http.NewServeMux()
 	userModel := users.NewModel(db)
 
-    staticFileServer := http.FileServer(http.Dir("./static"))
-    mux.Handle("GET /static/", http.StripPrefix("/static", staticFileServer))
+	staticFileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", staticFileServer))
 
-    userService := users.NewService(userModel)
-    userService.Register(mux)
+	userService := users.NewService(userModel, sessionStore)
+	userService.Register(mux)
 
-    server := http.Server{
-        Addr: fmt.Sprintf(":%d", port),
-        Handler: mux,
-        IdleTimeout: time.Minute,
+	server := http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
-    }
+	}
 
-    fmt.Printf("Listening on http://127.0.0.1:%d\n", port)
-    err = server.ListenAndServe()
-    if (err != nil) {
-        panic(err)
-    }
+	fmt.Printf("Listening on http://127.0.0.1:%d\n", port)
+	err = server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
--- internal/users/service.go
+++ internal/users/service.go
@@ -5,19 +5,21 @@ import (
 
 	"github.com/a-h/templ"
 	"github.com/donseba/go-htmx"
+	"github.com/gorilla/sessions"
 )
 
 type Service struct {
 	model UserModel
 	htmx  *htmx.HTMX
+    sessionStore *sessions.CookieStore
 }
 
-func NewService(model UserModel) Service {
-	return Service{model, htmx.New()}
+func NewService(model UserModel, sessionStore *sessions.CookieStore) Service {
+	return Service{model, htmx.New(), sessionStore}
 }
 
 func (s Service) Register(mux *http.ServeMux) {
-	mux.Handle("/", templ.Handler(home()))
+	mux.HandleFunc("/", s.homePage)
 	mux.Handle("GET /login", templ.Handler(login()))
 	mux.Handle("GET /sign-up", templ.Handler(signUp()))
 	mux.HandleFunc("POST /sign-up", s.signUpPost)
